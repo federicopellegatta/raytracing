7f519a52962d2eb7a98c3085b12ce2cff2cc9802
pyros-cyber <simonepirota@gmail.com>
2021-03-10 12:08:41 +0100

    Add Color.is_close()

--- src/colors.cpp
+++ src/colors.cpp
@@ -4,3 +4,16 @@
 #include <iostream>
 
 using namespace std;
+
+int main() {
+
+  bool Color::is_close(Color b) {
+    return Color::are_close(this.r, b.r) && Color::are_close(this.b, b.b) &&
+           Color::are_close(this.g, b.g);
+  }
+
+  bool Color::are_close(float a, float b) {
+    float eps = 1e-10;
+    return abs(a, b) < eps;
+  }
+}
