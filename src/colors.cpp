cbd071aa264e32181a653dbeda7c3a33337de10c
Federico Pellegatta <pellegatta.federico@gmail.com>
2021-03-10 12:05:41 +0100

    Add Color methods

--- src/colors.cpp
+++ src/colors.cpp
@@ -4,3 +4,23 @@
 #include <iostream>
 
 using namespace std;
+
+Color::Color(float r, float g, float b) {
+  Color.r = r;
+  Color.g = g;
+  Color.b = b;
+}
+
+Color::~Color(){};
+
+Color::operator+(Color a, Color b) {
+  return Color(a.r + b.r, a.g + b.g, a.b + b.b);
+}
+
+Color::operator*(float a, Color b) { return Color(a * b.r, a * b.g, a * b.b); }
+
+Color::operator*(Color b, float a) { return Color(a * b.r, a * b.g, a * b.b); }
+
+Color::operator*(Color a, Color b) {
+  return Color(a.r * b.r, a.g * b.g, a.b * b.b);
+}
