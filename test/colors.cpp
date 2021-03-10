9b3607ed7f3b10e8c023ec77d592541192df5b55
Federico Pellegatta <pellegatta.federico@gmail.com>
2021-03-10 12:44:12 +0100

    Fix Test

--- test/colors.cpp
+++ test/colors.cpp
@@ -4,7 +4,7 @@
 int main() {
   Color c1(1.0, 2.0, 3.0);
   Color c2(5.0, 7.0, 9.0);
-  Color c3(6.0, 9.0, 11.0);
+  Color c3(6.0, 9.0, 12.0);
 
   return c3.is_close(c1 + c2) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
