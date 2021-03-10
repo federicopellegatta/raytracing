e62c85246e5f8402fcd218c2b678ad1e3a6d792c
pyros-cyber <simonepirota@gmail.com>
2021-03-10 12:30:35 +0100

    Add test

--- test/colors.cpp
+++ test/colors.cpp
@@ -1,1 +1,10 @@
-// test/colors.cpp
+#include "../include/colors.h"
+#include <cstdlib>
+
+int main() {
+  Color c1(1.0, 2.0, 3.0);
+  Color c2(5.0, 7.0, 9.0);
+  Color c3(6.0, 9.0, 11.0);
+
+  return c3.is_close(c1 + c2) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
