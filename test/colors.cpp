32c491a2a956f121f8a7cfe497a1466ba2332766
Federico Pellegatta <pellegatta.federico@gmail.com>
2021-03-10 16:27:24 +0100

    Automated testing upgraded

--- test/colors.cpp
+++ test/colors.cpp
@@ -4,7 +4,14 @@
 int main() {
   Color c1(1.0, 2.0, 3.0);
   Color c2(5.0, 7.0, 9.0);
-  Color c3(6.0, 9.0, 12.0);
+  float a = 3.0;
 
-  return c3.is_close(c1 + c2) ? EXIT_SUCCESS : EXIT_FAILURE;
+  Color c3(6.0, 9.0, 12.0);
+  Color c4(3.0, 6.0, 9.0);
+  Color c5(5.0, 14.0, 27.1);
+
+  return (c3.is_close(c1 + c2) && c4.is_close(c1 * a) && c5.is_close(c1 * c2))
+             ? EXIT_SUCCESS
+             : EXIT_FAILURE;
+  // return c3.is_close(c1 + c2) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
