8f7f1b034ff69d76996b96e5bdba4025170ed53a
Federico Pellegatta <pellegatta.federico@gmail.com>
2021-03-10 11:46:30 +0100

    Colors Struct First Version

--- include/colors.h
+++ include/colors.h
@@ -1,1 +1,19 @@
 // include/colors.h
+
+struct Color {
+public:
+  float r, b, g;
+
+  Color(float, float, float);
+  ~Color();
+
+  Color operator+(Color, Color);
+  Color operator*(float, Color);
+  Color operator*(Color, float);
+  Color operator*(Color, Color);
+
+  bool are_close(Color, Color);
+
+private:
+  bool are_close(float a, float b, float epsilon);
+};
--- src/colors.cpp
+++ src/colors.cpp
@@ -1,1 +1,6 @@
 // src/colors.cpp
+
+#include "../include/colors.h"
+#include <iostream>
+
+using namespace std;
