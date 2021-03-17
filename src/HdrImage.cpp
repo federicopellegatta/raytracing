318e91ad28c771528e1c6953168536ab2a4de52d
pyros-cyber <simonepirota@gmail.com>
2021-03-17 11:15:04 +0100

    Update HdrImage.cpp

--- src/HdrImage.cpp
+++ src/HdrImage.cpp
@@ -1,3 +1,9 @@
-#include "../include/colors.h"
+#include "../include/HdrImage.h"
 
 using namespace std;
+
+void HdrImage::set_pixel(unsigned int x, unsigned int y, Color color) {
+  if (_valid_coordinates(x, y)) {
+    pixels[_pixel_offset(x, y)] = color;
+  }
+}
