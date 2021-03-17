d8995352a5aefd1c717574505bc1f9a217ec7007
Federico Pellegatta <pellegatta.federico@gmail.com>
2021-03-17 11:14:12 +0100

    Add valid_cordinates and pixel_offset methods

--- src/HdrImage.cpp
+++ src/HdrImage.cpp
@@ -1,3 +1,22 @@
-#include "../include/colors.h"
+#include "../include/HdrImage.h"
 
 using namespace std;
+
+HdrImage::HdrImage(unsigned int w, unsigned int h) {
+  width = w;
+  height = h;
+}
+
+HdrImage::~HdrImage(){};
+
+bool HdrImage::_valid_coordinates(unsigned int x, unsigned int y) {
+  if (x < width && y < height) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+unsigned int HdrImage::_pixel_offset(unsigned int x, unsigned int y) {
+  return y * height + x;
+}
