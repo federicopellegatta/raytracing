fe5e68f83d1c13e57cc3feaebaffcb1d6074388c
Federico Pellegatta <pellegatta.federico@gmail.com>
2021-03-22 18:37:19 +0100

    New methods added: write_float, endianness, write_pfm

--- include/HdrImage.h
+++ include/HdrImage.h
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+enum class Endianness { little_endian, big_endian };
+
 struct HdrImage {
   int width;
   int height;
@@ -17,9 +19,13 @@ struct HdrImage {
 
   Color get_pixel(int, int);
   void set_pixel(int, int, Color);
-  void write_pfm(ofstream &stream, float value);
-  void write_pfm(ostringstream &stream, float value);
+  void write_pfm(ofstream &stream, Endianness endianness);
+  // void write_pfm(ostringstream &stream, Endianness endianness);
+  void
+  write_float(ofstream &stream, float value,
+              Endianness endianness); // scompone un float nei suoi quattro byte
 
   bool valid_coordinates(int, int);
   int pixel_offset(int, int);
+  float endianness(Endianness);
 };
--- src/HdrImage.cpp
+++ src/HdrImage.cpp
@@ -1,5 +1,4 @@
 #include "../include/HdrImage.h"
-#include <iostream>
 
 using namespace std;
 
@@ -29,3 +28,54 @@ void HdrImage::set_pixel(int x, int y, Color color) {
 }
 
 Color HdrImage::get_pixel(int x, int y) { return pixels[pixel_offset(x, y)]; }
+
+void HdrImage::write_float(ofstream &stream, float value,
+                           Endianness endianness) {
+  // Convert "value" in a sequence of 32 bit
+  uint32_t double_word{*((uint32_t *)&value)};
+
+  // Extract the four bytes in "double_word" using bit-level operators
+  uint8_t bytes[] = {
+      static_cast<uint8_t>(double_word & 0xFF), // Least significant byte
+      static_cast<uint8_t>((double_word >> 8) & 0xFF),
+      static_cast<uint8_t>((double_word >> 16) & 0xFF),
+      static_cast<uint8_t>((double_word >> 24) & 0xFF), // Most significant byte
+  };
+
+  switch (endianness) {
+  case Endianness::little_endian:
+    for (int i{}; i < 4; ++i) // Forward loop
+      stream << bytes[i];
+    break;
+
+  case Endianness::big_endian:
+    for (int i{3}; i >= 0; --i) // Backward loop
+      stream << bytes[i];
+    break;
+  }
+}
+
+float HdrImage::endianness(Endianness e) {
+  if (e == Endianness::little_endian) {
+    return -1.0;
+  } else {
+    return 1.0;
+  }
+}
+
+void HdrImage::write_pfm(ofstream &stream, Endianness e) {
+  stringstream sstr;
+
+  sstr << "PF\n" << width << " " << height << "\n" << endianness(e);
+  string result{sstr.str()};
+
+  stream << result << endl;
+  for (int y = height - 1; y >= 0; y--) {
+    for (int x = 0; x < width; x++) {
+      Color color = get_pixel(x, y);
+      write_float(stream, color.r, e);
+      write_float(stream, color.g, e);
+      write_float(stream, color.b, e);
+    }
+  }
+}
