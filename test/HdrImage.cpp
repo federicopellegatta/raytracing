b1bda8276ea3d1b8bf2c48bc51452696c1114700
Federico Pellegatta <pellegatta.federico@gmail.com>
2021-03-22 19:54:27 +0100

    test_pfm_save first draft deployed

--- test/HdrImage.cpp
+++ test/HdrImage.cpp
@@ -25,9 +25,39 @@ void test_set_get_pixel(HdrImage img, Color color, int x, int y) {
   assert(color.is_close(img.get_pixel(x, y)));
 }
 
+void test_pfm_save(HdrImage img) { // SOLO little_endian
+  // set pixels of my image
+  img.set_pixel(0, 0, Color(1.0e1, 2.0e1, 3.0e1));
+  img.set_pixel(1, 0, Color(4.0e1, 5.0e1, 6.0e1));
+  img.set_pixel(2, 0, Color(7.0e1, 8.0e1, 9.0e1));
+  img.set_pixel(0, 1, Color(1.0e2, 2.0e2, 3.0e2));
+  img.set_pixel(1, 1, Color(4.0e2, 5.0e2, 6.0e2));
+  img.set_pixel(2, 1, Color(7.0e2, 8.0e2, 9.0e2));
+
+  ofstream outputFile("my_le_img.pfm");
+
+  img.write_pfm(
+      outputFile,
+      Endianness::little_endian); // perchè non crea nessun file output quando
+                                  // eseguo ./HdrImageTest???
+
+  // little_endian reference pfm and compare it with my image
+  unsigned char reference_le_pfm[] = {
+      0x50, 0x46, 0x0a, 0x33, 0x20, 0x32, 0x0a, 0x2d, 0x31, 0x2e, 0x30, 0x0a,
+      0x00, 0x00, 0xc8, 0x42, 0x00, 0x00, 0x48, 0x43, 0x00, 0x00, 0x96, 0x43,
+      0x00, 0x00, 0xc8, 0x43, 0x00, 0x00, 0xfa, 0x43, 0x00, 0x00, 0x16, 0x44,
+      0x00, 0x00, 0x2f, 0x44, 0x00, 0x00, 0x48, 0x44, 0x00, 0x00, 0x61, 0x44,
+      0x00, 0x00, 0x20, 0x41, 0x00, 0x00, 0xa0, 0x41, 0x00, 0x00, 0xf0, 0x41,
+      0x00, 0x00, 0x20, 0x42, 0x00, 0x00, 0x48, 0x42, 0x00, 0x00, 0x70, 0x42,
+      0x00, 0x00, 0x8c, 0x42, 0x00, 0x00, 0xa0, 0x42, 0x00, 0x00, 0xb4, 0x42};
+
+  // come faccio confronto?
+}
+
 int main() {
 
   HdrImage img(7, 4);
+  HdrImage img1(3, 2);
   Color reference_color(1.0, 2.0, 3.0);
 
   // Testing if set and get pixel works properly
@@ -37,6 +67,8 @@ int main() {
   test_coordinates(img);
   test_pixel_offset(img);
   test_set_get_pixel(img, reference_color, 3, 2);
+
+  void test_pfm_save(HdrImage img1);
 
   return 0;
 }
