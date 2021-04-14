d9b3f43cb9e6df2e78e48cfd98808114742cddb4
Federico Pellegatta <pellegatta.federico@gmail.com>
2021-04-14 12:35:02 +0200

    Add to_str and are_close methods

--- include/geometry.h
+++ include/geometry.h
@@ -1,6 +1,16 @@
+#include "colors.h"
 #include <iostream>
 
 using namespace std;
+
+template <typename In> string convert_to_string(const In &a) {
+  return string{"(" + to_string(a.x) + ", " + to_string(a.y) + ", " +
+                to_string(a.z) + ")"};
+}
+
+template <typename In> bool are_close(const In &a, const In &b) {
+  return are_close(a.x, b.x) && are_close(a.y, b.y) && are_close(a.z, b.z);
+}
 
 struct Vec {
   float x, y, z;
@@ -8,13 +18,18 @@ struct Vec {
   Vec(float _x = 0, float _y = 0, float _z = 0) : x{_x}, y{_y}, z{_z} {}
   Vec(const Vec &);  // Copy constructor
   Vec(const Vec &&); // Move constructor
+  inline string to_str() { return string{"Vec" + convert_to_string(*this)}; }
+  inline bool is_close(Vec a) { return are_close<Vec>(*this, a); }
 };
+
 struct Point {
   float x, y, z;
 
   Point(float _x = 0, float _y = 0, float _z = 0) : x{_x}, y{_y}, z{_z} {}
   Point(const Point &);  // Copy constructor
   Point(const Point &&); // Move constructor
+  inline string to_str() { return string{"Point" + convert_to_string(*this)}; }
+  inline bool is_close(Point a) { return are_close<Point>(*this, a); }
 };
 
 template <typename In1, typename In2, typename Out>
