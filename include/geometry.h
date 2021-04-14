283ceae693f2b7b282ed07613fc85dc580f75a5d
pyros-cyber <simonepirota@gmail.com>
2021-04-14 11:29:12 +0200

    Add sum template

--- include/geometry.h
+++ include/geometry.h
@@ -9,7 +9,6 @@ struct Vec {
   Vec(const Vec &);  // Copy constructor
   Vec(const Vec &&); // Move constructor
 };
-
 struct Point {
   float x, y, z;
 
@@ -17,3 +16,14 @@ struct Point {
   Point(const Point &);  // Copy constructor
   Point(const Point &&); // Move constructor
 };
+
+template <typename In1, typename In2, typename Out>
+Out _sum(const In1 &a, const In2 &b) {
+  return Out{a.x + b.x, a.y + b.y, a.z + b.z};
+}
+
+Point operator+(const Point &a, const Vec &b) {
+  return _sum<Point, Vec, Point>(a, b);
+}
+
+Vec operator+(const Vec &a, const Vec &b) { return _sum<Vec, Vec, Vec>(a, b); }
