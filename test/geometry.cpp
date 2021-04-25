b40e1cf95ad42ecbb412897ccf18a5df20546f36
pyros-cyber <simonepirota@gmail.com>
2021-04-25 13:58:07 +0200

    Add transformation constructor test

--- test/geometry.cpp
+++ test/geometry.cpp
@@ -2,9 +2,35 @@
 #include <cassert>
 #include <cstdlib>
 
-using namespace fmt;
+using namespace std;
 
+//////////////////////////////
+/* TRANSFORMATION UNIT TESTS*/
+//////////////////////////////
+
+void test_transformation_constructor(float m1[4][4], float minv1[4][4]) {
+  Transformation t1{m1, minv1};
+  assert(t1.is_consistent());
+  // fmt::print(t1.to_str());
+  Transformation t2{m1, minv1};
+  assert(t1.is_close(t2));
+  Transformation t3{m1, minv1};
+  t3.m[2][2] += 1.0; // making t3 not consistent
+  assert(!t1.is_close(t3));
+  Transformation t4{m1, minv1};
+  t4.m[2][2] += 1.0; // making t4 not consistent
+  assert(!t1.is_close(t4));
+}
+
+void test_transformation_mul(float m1[4][4], float minv1[4][4]) {
+  Transformation t1{m1, minv1};
+}
 int main() {
+
+  /////////////////////////////////
+  /* VECS, POINT AND NORMAL TESTS*/
+  /////////////////////////////////
+
   Vec a(1.0, 2.0, 3.0);
   Vec b(4.0, 6.0, 8.0);
   Point p(1.0, 2.0, 3.0);
@@ -33,7 +59,9 @@ int main() {
   assert(q + a == Point(5.0, 8.0, 11.0));
   assert(q - p == Vec(3.0, 4.0, 5.0));
 
-  // check Matrix constructor
+  /////////////////////////
+  /* TRANSFORMATION TESTS*/
+  /////////////////////////
   float m1[4][4] = {{1.0, 2.0, 3.0, 4.0},
                     {5.0, 6.0, 7.0, 8.0},
                     {9.0, 9.0, 8.0, 7.0},
@@ -43,10 +71,7 @@ int main() {
                        {0.5, 0.5, -1.0, 1.0},
                        {-1.375, 0.875, 0.0, -0.5}};
 
-  Transformation t1{m1, minv1};
-  assert(t1.is_consistent());
-
-  print(t1.to_str());
+  test_transformation_constructor(m1, minv1);
 
   return 0;
 }
