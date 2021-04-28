82070ed3e27fbcfa8a44ae755d9b7547fba2cf64
Federico Pellegatta <pellegatta.federico@gmail.com>
2021-04-28 12:09:24 +0200

    Deploy Ray

--- include/ray.h
+++ include/ray.h
@@ -4,6 +4,18 @@
 
 using namespace std;
 
+/**
+ * @brief A ray of light propagating in space
+ * The class contains the following members:
+    -   `origin` (``Point``): the 3D point where the ray originated
+    -   `dir` (``Vec``): the 3D direction along which this ray propagates
+    -   `tmin` (float): the minimum distance travelled by the ray is this number
+ times `dir`
+    -   `tmax` (float): the maximum distance travelled by the ray is this number
+ times `dir`
+    -   `depth` (int): number of times this ray was reflected/refracted"""
+
+ */
 struct Ray {
   Point origin = Point();
   Vec dir = Vec();
@@ -12,8 +24,38 @@ struct Ray {
   int depth = 0;
 
   Ray();
+  Ray(Point _origin, Vec _dir, float _tmin, float _tmax, int _depth) {
+    origin = _origin;
+    dir = _dir;
+    tmin = _tmin;
+    tmax = _tmax;
+    depth = _depth;
+  }
 
-  bool is_close(Ray other) {
+  inline bool is_close(Ray other) {
     return (origin == other.origin) && (dir == other.dir);
+  }
+
+  /**
+   * @brief Compute the point along the ray's path at some distance from the
+   * origin
+   *
+   * @param t Lenght in `dir` unit
+   * @return Point The point in 3D space whose distance from the
+        ray's origin is equal to `t`
+   */
+  inline Point at(float t) { return origin + dir * t; }
+
+  /**
+   * @brief Transform a ray
+    This method returns a new ray whose origin and direction are the
+   transformation of the original ray
+   *
+   * @param transformation
+   * @return Ray
+   */
+  Ray transform(Transformation transformation) {
+    return Ray(transformation * origin, transformation * dir, tmin, tmax,
+               depth);
   }
 };
