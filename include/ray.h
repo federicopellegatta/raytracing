cd2f1b15408abe67fef64bbab2965f54bdd25c27
Federico Pellegatta <pellegatta.federico@gmail.com>
2021-04-28 11:36:36 +0200

    Change camera.h->ray.h

--- include/ray.h
+++ include/ray.h
@@ -4,10 +4,16 @@
 
 using namespace std;
 
-struct ray {
-  Point origin;
-  Vec direction;
+struct Ray {
+  Point origin = Point();
+  Vec dir = Vec();
   float tmin = 1e-5;
   float tmax = numeric_limits<float>::infinity();
   int depth = 0;
+
+  Ray();
+
+  bool is_close(Ray other) {
+    return (origin == other.origin) && (dir == other.dir);
+  }
 };
