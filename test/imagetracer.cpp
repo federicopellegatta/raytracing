13a31ebec6bf3d27fe5e95e981b1b8245ab3b915
Federico Pellegatta <48312200+federicopellegatta@users.noreply.github.com>
2021-05-05 10:59:40 +0200

    Fix issue #3 (#4)
    * Add orientation test in order to fix Issue #3
    
    * Fix issue #3

--- test/imagetracer.cpp
+++ test/imagetracer.cpp
@@ -3,33 +3,39 @@
 
 using namespace std;
 
-void test_imagetracer() {
-  HdrImage img(4, 2);
-  //  PerspectiveCamera cam(1.0, 2.0, Transformation());
-  shared_ptr<Camera> camera =
-      make_shared<PerspectiveCamera>(1.0, 2.0, Transformation());
-  ImageTracer tracer(img, camera);
-
+void test_uv_sub_mapping(ImageTracer tracer) {
   Ray ray1 = tracer.fire_ray(0, 0, 2.5, 1.5);
   Ray ray2 = tracer.fire_ray(2, 1, 0.5, 0.5);
   assert(ray1 == ray2);
+}
 
-  /*
-  Color lambda_func = [](Ray ray) -> Color { return Color(1.0, 2.0, 3.0); };
-  Color test_lambda = lambda_func;
-  fmt::print("Test if lambda is working \n r = {}, g = {}, b = {} \n",
-             test_lambda.r, test_lambda.g, test_lambda.b);
-  */
+void test_orientation(ImageTracer tracer) {
+  Ray top_left_ray = tracer.fire_ray(0, 0, 0.0, 0.0);
+  assert(Point(0.0, 2.0, 1.0) == top_left_ray.at(1.0));
 
+  // Fire a ray against bottom - right corner of the screen
+  Ray bottom_right_ray = tracer.fire_ray(3, 1, 1.0, 1.0);
+  assert(Point(0.0, -2.0, -1.0) == bottom_right_ray.at(1.0));
+}
+
+void test_image_coverage(ImageTracer tracer) {
   tracer.fire_all_rays([](Ray ray) -> Color { return Color(1.0, 2.0, 3.0); });
-  for (int row{}; row < img.height; row++) {
-    for (int col{}; col < img.width; col++) {
+  for (int row{}; row < tracer.image.height; row++) {
+    for (int col{}; col < tracer.image.width; col++) {
       assert(tracer.image.get_pixel(col, row).is_close(Color(1.0, 2.0, 3.0)));
     }
   }
 }
 int main() {
-  test_imagetracer();
+
+  HdrImage img(4, 2);
+  shared_ptr<Camera> camera =
+      make_shared<PerspectiveCamera>(1.0, 2.0, Transformation());
+  ImageTracer tracer(img, camera);
+
+  test_uv_sub_mapping(tracer);
+  test_image_coverage(tracer);
+  test_orientation(tracer);
 
   return 0;
 }
