9f550376bb64d7f1476ef4ebc2f6aa6ad0eb3324
Federico Pellegatta <pellegatta.federico@gmail.com>
2021-05-05 13:04:52 +0200

    First version

--- include/world.h
+++ include/world.h
@@ -1,5 +1,33 @@
 #ifndef WORLD_H
 #define WORLD_H
-struct world {};
+
+#include "shapes.h"
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+struct World {
+  vector<shared_ptr<Shape>> shapes;
+
+  void add(shared_ptr<Shape> newShape) {
+    shapes.push_back(make_shared<Shape>(newShape));
+  }
+
+  HitRecord rayIntersection(Ray ray) {
+    HitRecord closest{};
+
+    for (int i{}; i < shapes.size(); i++) {
+      HitRecord intersection = shapes[i]->ray_intersection(ray);
+      if (!intersection.hit)
+        continue;
+      if ((!closest.hit) || (intersection.t < closest.t))
+        HitRecord closest{intersection};
+    }
+
+    return closest;
+  }
+};
 
 #endif
