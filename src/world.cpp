7d46ddf12958b6e17e08d22ff6607b6ff13c4afa
pyros-cyber <simonepirota@gmail.com>
2021-05-09 20:03:03 +0200

    Move code implementation to source file

---
+++ src/world.cpp
@@ -0,0 +1,16 @@
+#include "world.h"
+#include "ray.h"
+
+HitRecord World::ray_intersection(Ray ray) {
+  HitRecord closest{};
+
+  for (int i{}; i < shapes.size(); i++) {
+    HitRecord intersection = shapes[i]->ray_intersection(ray);
+    if (!intersection.hit)
+      continue;
+    if ((!closest.hit) || (intersection.t < closest.t))
+      HitRecord closest{intersection};
+  }
+
+  return closest;
+}
