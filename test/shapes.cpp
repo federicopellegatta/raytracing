779b37aa81b4744379655b162995282cc42dfd13
pyros-cyber <simonepirota@gmail.com>
2021-05-09 20:03:57 +0200

    Add first test, which is failing :(

--- test/shapes.cpp
+++ test/shapes.cpp
@@ -1,3 +1,19 @@
 #include "shapes.h"
+#include <cassert>
 
-int main() { return 0; }
+using namespace std;
+
+void test_sphere_hit() {
+  Sphere sphere;
+  Ray ray1(Point(0., 0., 1.), -1 * VEC_Z);
+
+  HitRecord intersection1 = sphere.ray_intersection(ray1);
+  assert(intersection1.hit);
+  assert(HitRecord(Point(0., 0., 1.), Normal(0., 0., 1.), Vec2d(0., 0.), 1.0,
+                   ray1, false)
+             .is_close(intersection1));
+}
+int main() {
+  test_sphere_hit();
+  return 0;
+}
