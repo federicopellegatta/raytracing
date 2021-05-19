81ad63879670b6ec4361808cda539d9595aeb20e
pyros-cyber <simonepirota@gmail.com>
2021-05-19 11:36:36 +0200

    add BRDF and MATERIAL classes

--- include/materials.h
+++ include/materials.h
@@ -22,7 +22,47 @@ struct CheckeredPigment : public Pigment {
 
   CheckeredPigment(Color _color1 = Color(), Color _color2 = Color(),
                    int _num_of_steps = 10)
-      : color1{_color1}, color2{_color2}, num_of_steps{_num_of_steps} {}
+      : color1{_color1}, color2{_color2} {
+    num_of_steps = _num_of_steps;
+  }
   Color get_color(Vec2d uv);
+};
+
+struct BRDF {
+  shared_ptr<Pigment> pigment;
+
+  BRDF(shared_ptr<Pigment> _pigment = make_shared<UniformPigment>(Color(1., 1.,
+                                                                        1.)))
+      : pigment{_pigment} {}
+
+  inline Color eval(Normal normal, Vec inc_dir, Vec out_dir, Vec2d uv) {
+    return Color(0., 0., 0.);
+  }
+};
+
+struct DiffusiveBRDF : public BRDF {
+  shared_ptr<Pigment> pigment;
+  float reflectance;
+
+  DiffusiveBRDF(shared_ptr<Pigment> _pigment =
+                    make_shared<UniformPigment>(Color(1., 1., 1.)),
+                float _reflectance = 1.)
+      : pigment{_pigment} {
+    reflectance = _reflectance;
+  }
+
+  inline Color eval(Normal normal, Vec inc_dir, Vec out_dir, Vec2d uv) {
+    return pigment->get_color(uv) * (reflectance / M_PI);
+  }
+};
+
+struct Material {
+  shared_ptr<BRDF> brdf;
+  shared_ptr<Pigment> emitted_radiance;
+
+  Material(shared_ptr<BRDF> _brdf = make_shared<DiffusiveBRDF>(),
+           shared_ptr<Pigment> _emitted_radiance =
+               make_shared<UniformPigment>(Color(0., 0., 0.)))
+      : brdf{_brdf}, emitted_radiance{_emitted_radiance} {}
 };
 #endif
