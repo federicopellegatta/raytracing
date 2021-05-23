69cfee9b5ab8e519f486b7f81a90d38e84b3ee8a
pyros-cyber <simonepirota@gmail.com>
2021-05-23 17:20:44 +0200

    add code docs

--- include/render.h
+++ include/render.h
@@ -7,6 +7,11 @@
 #include "ray.h"
 #include "world.h"
 
+/**
+ * @brief A class implementing a solver of the rendering equation
+ * This is an abstract class.
+ *
+ */
 struct Renderer {
   World world;
   Color background_color;
@@ -14,9 +19,19 @@ struct Renderer {
   Renderer(World _world, Color _background_color = BLACK)
       : world{_world}, background_color{_background_color} {};
 
+  /**
+   * @brief Estimante a radiance along a Ray
+   *
+   * @return Color
+   */
   virtual Color operator()(Ray) = 0;
 };
 
+/**
+ * @brief An on/off renderer, mostly useful for debugging purposes, because it
+ * is really fast but produce only black and white images
+ *
+ */
 struct OnOffRenderer : public Renderer {
   Color color;
   OnOffRenderer(World world, Color background_color = BLACK,
@@ -28,11 +43,16 @@ struct OnOffRenderer : public Renderer {
   }
 };
 
+/**
+ * @brief A "flat" renderer, that estimates the solution of the rendering
+ equation by neglecting any contribution of the light. It just uses the pigment
+ of each surface to determine how to compute the final radiance.
+ *
+ */
 struct FlatRenderer : public Renderer {
   FlatRenderer(World world, Color background_color = BLACK)
       : Renderer(world, background_color){};
 
   Color operator()(Ray ray);
 };
-
 #endif
