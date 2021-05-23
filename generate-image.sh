d68cd11af6ec244eade6e6ffee27d6ebffd78eaa
pyros-cyber <simonepirota@gmail.com>
2021-05-23 18:09:57 +0200

    add script to use with `parallel` command

---
+++ generate-image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+    echo "Usage: $(basename $0) ANGLE"
+    exit 1
+fi
+
+readonly angle="$1"
+readonly angleNNN=$(printf "%03d" $angle)
+readonly pfmfile=image$angleNNN.pfm
+readonly pngfile=image$angleNNN.png
+
+#time ./main.py demo --algorithm flat --angle-deg $angle \
+#    --width 640 --height 480 --pfm-output $pfmfile \
+#    && ./main.py pfm2png --luminosity 0.5 $pfmfile $pngfile
+time ./raytracer demo --width 640 --height 480 --deg $angle \
+	--outf $pfmfile \
+	&& ./raytracer convertpfm2png --luminosity 0.5 $pfmfile $pngfile
