d78c9ba11acf9b609490351a8c47486ee9edca23
pyros-cyber <simonepirota@gmail.com>
2021-05-24 19:43:04 +0200

    modify scripts

--- generate-image.sh
+++ generate-image.sh
@@ -13,6 +13,7 @@ readonly pngfile=image$angleNNN.png
 #time ./main.py demo --algorithm flat --angle-deg $angle \
 #    --width 640 --height 480 --pfm-output $pfmfile \
 #    && ./main.py pfm2png --luminosity 0.5 $pfmfile $pngfile
-time ./raytracer demo --width 640 --height 480 --deg $angle \
-	--outf $pfmfile \
-	&& ./raytracer convertpfm2png --luminosity 0.5 $pfmfile $pngfile
+time ../raytracer demo --width 640 --height 480 --deg $angle \
+	--algorithm flat --outf $pfmfile \
+	&& ../raytracer convertpfm2png --factor 1 --gamma 1 --luminosity 0.5 \
+        --inpfm $pfmfile --outpng $pngfile
