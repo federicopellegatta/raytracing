664a30ddfc5e140b1ddd10588418bf78b0d1b7c4
pyros-cyber <simonepirota@gmail.com>
2021-05-24 15:32:17 +0200

    change image to tracer.image in asserts

--- test/render.cpp
+++ test/render.cpp
@@ -19,19 +19,17 @@ void test_OnOff_render() {
   OnOffRenderer renderer(world);
   tracer.fire_all_rays(renderer);
 
-  assert(image.get_pixel(0, 0).is_close(BLACK));
-  assert(image.get_pixel(1, 0).is_close(BLACK));
-  assert(image.get_pixel(2, 0).is_close(BLACK));
+  assert(tracer.image.get_pixel(0, 0).is_close(BLACK));
+  assert(tracer.image.get_pixel(1, 0).is_close(BLACK));
+  assert(tracer.image.get_pixel(2, 0).is_close(BLACK));
 
-  assert(image.get_pixel(0, 1).is_close(BLACK));
-  cout << image.get_pixel(1, 1).r << " " << image.get_pixel(1, 1).g << " "
-       << image.get_pixel(1, 1).b << " " << endl;
-  assert(image.get_pixel(1, 1).is_close(WHITE));
-  assert(image.get_pixel(2, 1).is_close(BLACK));
+  assert(tracer.image.get_pixel(0, 1).is_close(BLACK));
+  assert(tracer.image.get_pixel(1, 1).is_close(WHITE));
+  assert(tracer.image.get_pixel(2, 1).is_close(BLACK));
 
-  assert(image.get_pixel(0, 2).is_close(BLACK));
-  assert(image.get_pixel(1, 2).is_close(BLACK));
-  assert(image.get_pixel(2, 2).is_close(BLACK));
+  assert(tracer.image.get_pixel(0, 2).is_close(BLACK));
+  assert(tracer.image.get_pixel(1, 2).is_close(BLACK));
+  assert(tracer.image.get_pixel(2, 2).is_close(BLACK));
 }
 
 void test_flat_render() {
@@ -48,24 +46,22 @@ void test_flat_render() {
   FlatRenderer renderer(world);
   tracer.fire_all_rays(renderer);
 
-  assert(image.get_pixel(0, 0).is_close(BLACK));
-  assert(image.get_pixel(1, 0).is_close(BLACK));
-  assert(image.get_pixel(2, 0).is_close(BLACK));
+  assert(tracer.image.get_pixel(0, 0).is_close(BLACK));
+  assert(tracer.image.get_pixel(1, 0).is_close(BLACK));
+  assert(tracer.image.get_pixel(2, 0).is_close(BLACK));
 
-  assert(image.get_pixel(0, 1).is_close(BLACK));
-  cout << image.get_pixel(1, 1).r << " " << image.get_pixel(1, 1).g << " "
-       << image.get_pixel(1, 1).b << " " << endl;
-  assert(image.get_pixel(1, 1).is_close(sphere_color));
-  assert(image.get_pixel(2, 1).is_close(BLACK));
+  assert(tracer.image.get_pixel(0, 1).is_close(BLACK));
+  assert(tracer.image.get_pixel(1, 1).is_close(sphere_color));
+  assert(tracer.image.get_pixel(2, 1).is_close(BLACK));
 
-  assert(image.get_pixel(0, 2).is_close(BLACK));
-  assert(image.get_pixel(1, 2).is_close(BLACK));
-  assert(image.get_pixel(2, 2).is_close(BLACK));
+  assert(tracer.image.get_pixel(0, 2).is_close(BLACK));
+  assert(tracer.image.get_pixel(1, 2).is_close(BLACK));
+  assert(tracer.image.get_pixel(2, 2).is_close(BLACK));
 }
 
 int main() {
   test_OnOff_render();
-  // test_flat_render();
+  test_flat_render();
 
   return 0;
 }
