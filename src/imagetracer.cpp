ca3a3d5afb043a3224adda6192ab6b13dd4f1384
pyros-cyber <simonepirota@gmail.com>
2021-06-08 12:52:06 +0200

    remove old code lines

--- src/imagetracer.cpp
+++ src/imagetracer.cpp
@@ -3,9 +3,6 @@
 void ImageTracer::fire_all_rays(function<Color(const Ray &)> func) {
   for (int row{}; row < image.height; row++) {
     for (int col{}; col < image.width; col++) {
-      // Ray ray = fire_ray(col, row);
-      // Color color = func(ray);
-      // image.set_pixel(col, row, color);
       Color cum_color;
 
       if (samples_per_side > 0) {
