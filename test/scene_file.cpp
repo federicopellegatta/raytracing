aeea34011cefc4819374208a0f85ac13315b2847
pyros-cyber <simonepirota@gmail.com>
2021-06-19 19:28:40 +0200

    Add first tests for `parser`

--- test/scene_file.cpp
+++ test/scene_file.cpp
@@ -85,8 +85,41 @@ void test_lexer() {
   assert(stream.read_token().type == TokenType::STOP);
 }
 
+void test_parser() {
+  stringstream sstr(
+      "\nfloat clock(150)\n\nmaterial "
+      "sky_material(\n\tdiffuse(uniform(<0, 0, 0>)),\n\tuniform(<0.7, 0.5, "
+      "1>)\n)\n\n# Here is a comment\n\nmaterial "
+      "ground_material(\n\tdiffuse(checkered(<0.3, "
+      "0.5, 0.1>,\n\t\t\t  <0.1, 0.2, 0.5>, 4)),\n\tuniform(<0, 0, "
+      "0>)\n)\n\nmaterial sphere_material(\n\tspecular(uniform(<0.5, 0.5, "
+      "0.5>)),\n\tuniform(<0, 0, 0>)\n)\n\nplane (sky_material, "
+      "translation([0, 0, 100]) * rotation_y(clock))\nplane (ground_material, "
+      "identity)\n\nsphere(sphere_material, translation([0, 0, "
+      "1]))\n\ncamera(perspective, rotation_z(30) * translation([-4, 0, 1]), "
+      "1.0, 2.0)\n");
+
+  // fmt::print(sstr.str());
+  InputStream stream(sstr);
+  map<string, float> vars;
+  Scene scene = stream._parse_scene(vars);
+
+  // Check that the float variables are ok
+
+  assert(scene.float_variables.size() == 1);
+  assert(scene.float_variables.find("clock") != scene.float_variables.end());
+  assert(scene.float_variables.at("clock") == 150.0);
+
+  // Check that the materials are ok
+  assert(scene.materials.size() == 3);
+  assert(scene.materials.find("sphere_material") != scene.materials.end());
+  assert(scene.materials.find("sky_material") != scene.materials.end());
+  assert(scene.materials.find("ground_material") != scene.materials.end());
+}
+
 int main() {
   test_input_file();
   test_lexer();
+  test_parser();
   return 0;
 }
