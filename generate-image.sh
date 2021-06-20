656d453257e99e154266316eb5c8b79b9a146a67
pyros-cyber <simonepirota@gmail.com>
2021-06-20 20:29:56 +0200

    Update scripts

--- generate-image.sh
+++ generate-image.sh
@@ -5,12 +5,13 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 
-readonly angle="$1"
-readonly angleNNN=$(printf "%03d" $angle)
+readonly angle_deg="$1"
+readonly angleNNN=$(printf "%03d" $angle_deg)
 readonly pfmfile=image$angleNNN.pfm
 readonly pngfile=image$angleNNN.png
 
-time ../raytracer demo --width 640 --height 480 --deg $angle \
-	--algorithm flat --outf $pfmfile \
-	&& ../raytracer convertpfm2png --factor 1 --gamma 1 --luminosity 0.5 \
+time ../raytracer render -w 640 -h 360 --alg pathtracing --num-of-rays 10 --max-depth 4 \
+        --init-state 42 --init-seq 54 --samples-per-pixel 4 \
+        --outf image$angleNNN -i ../../examples/demo.txt -d angle:$angle_deg \
+        && ../raytracer convertpfm2png --factor 1 --gamma 1 --luminosity 0.5 \
         --inpfm $pfmfile --outpng $pngfile
